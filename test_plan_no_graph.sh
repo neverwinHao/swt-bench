980aa90c197d051682575562e54c1fd176b8616e
neverwinHao <liuhao2025@ia.ac.cn>
2026-05-11 12:05:19 +0000

    eval version

--- test_plan_no_graph.sh
+++ test_plan_no_graph.sh
@@ -1,6 +1,15 @@
 python -m src.main \
-    --predictions_path "/home/v-haoliu3/SWT-RESULTS-PLAN/gpt-5-mini/plan_python_no_graph/full_preds.json" \
-    --max_workers 4 \
+    --predictions_path "/home/xinzhang3/haoliu/baselines-swt/e-otter/inference_outputs/claude_e_otter_plus_verified.json" \
+    --max_workers 12 \
     --cache_level "none" \
     --clean true \
-    --run_id "5-mini-plan-no-graph"
+    --run_id "claude_e_otter_plus_verified"
+
+
+
+python -m src.main \
+    --predictions_path "/home/xinzhang3/haoliu/gpt-5-no-th.json" \
+    --max_workers 12 \
+    --cache_level "none" \
+    --clean true \
+    --run_id "gpt-5-no-th"
